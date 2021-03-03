bae47a285ca0956050c35277fd7062af16873ac1
rfn900 <rfn900@gmail.com>
2021-03-03 21:04:40 +0100

    Add some script functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", e=>{
-    alert('halo bundao')
+    
+    const caretIconSpan = document.querySelectorAll(".caret-icon-span")
+    const channelList = document.querySelectorAll(".channel__list")
+    const toggleTitle = document.querySelectorAll(".toggle-title")
+    const toggleH3 = document.querySelector(".toggle-h3")
+    
+
+    toggleTitle.forEach((item,index)=>{
+        item.addEventListener('click', e => {
+            caretIconSpan[index].classList.toggle("rotate")
+            channelList[index].classList.toggle("hide")
+            toggleH3.classList.toggle("active")
+        })
+    })
 })
