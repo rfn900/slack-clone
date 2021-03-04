d2289dd2374c381bced0abcaac363b29d3212805
Rodrigo <rfn900@gmail.com>
2021-03-04 20:41:01 +0100

    Fix some indentation

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,15 @@
-document.addEventListener("DOMContentLoaded", e=>{
-    
-    const caretIconSpan = document.querySelectorAll(".caret-icon-span")
-    const channelList = document.querySelectorAll(".channel__list")
-    const toggleTitle = document.querySelectorAll(".toggle-title")
-    const toggleH3 = document.querySelector(".toggle-h3")
-    
+document.addEventListener("DOMContentLoaded", (e) => {
+  const caretIconSpan = document.querySelectorAll(".caret-icon-span");
+  const channelList = document.querySelectorAll(".channel__list");
+  const toggleTitle = document.querySelectorAll(".toggle-title");
+  const toggleH3 = document.querySelector(".toggle-h3");
 
-    toggleTitle.forEach((item,index)=>{
-        item.addEventListener('click', e => {
-            caretIconSpan[index].classList.toggle("rotate")
-            channelList[index].classList.toggle("hide")
-            if(index==0)toggleH3.classList.toggle("active")
-        })
-    })
-})
+  toggleTitle.forEach((item, index) => {
+    item.addEventListener("click", (e) => {
+      caretIconSpan[index].classList.toggle("rotate");
+      channelList[index].classList.toggle("hide");
+      if (index == 0) toggleH3.classList.toggle("active");
+    });
+  });
+});
+
