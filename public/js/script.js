c6f1e1c6b47b7eaa9604488c715b8d07a2c66598
rfn900 <rfn900@gmail.com>
2021-03-10 18:13:16 +0100

    Work on UI

--- public/js/script.js
+++ public/js/script.js
@@ -11,5 +11,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
       if (index == 0) toggleH3.classList.toggle("active");
     });
   });
-});
 
+  const mainChatInput = document.getElementById("main-chat-input");
+  const chatSubmitBtn = document.getElementById("chat-submit-button");
+  mainChatInput.addEventListener("keyup", (e) => {
+    console.log(mainChatInput.value);
+    if (mainChatInput.value != "") chatSubmitBtn.removeAttribute("disabled");
+    else chatSubmitBtn.setAttribute("disabled", true);
+  });
+});
