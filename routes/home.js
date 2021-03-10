e31851abfb9982d72234870a82f35ee4e4a807ef
rfn900 <rfn900@gmail.com>
2021-03-10 18:12:35 +0100

    Add icons

--- routes/home.js
+++ routes/home.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
-var library     = require('@fortawesome/fontawesome-svg-core').library;
-var dom         = require('@fortawesome/fontawesome-svg-core').dom;
-var icon        = require('@fortawesome/fontawesome-svg-core').icon;
-var fas         = require('@fortawesome/free-solid-svg-icons').fas;
-var far         = require('@fortawesome/free-regular-svg-icons').far
+var library = require("@fortawesome/fontawesome-svg-core").library;
+var dom = require("@fortawesome/fontawesome-svg-core").dom;
+var icon = require("@fortawesome/fontawesome-svg-core").icon;
+var fas = require("@fortawesome/free-solid-svg-icons").fas;
+var far = require("@fortawesome/free-regular-svg-icons").far;
 
-library.add(fas)
-library.add(far)
+library.add(fas);
+library.add(far);
 
-const homeIcon = icon({ prefix: 'fas', iconName: "home" }).html
-const searchIcon = icon({ prefix: 'fas', iconName: "search" }).html
-const arrowDown = icon({ prefix: 'fas', iconName: "chevron-down" }).html
-const atIcon = icon({ prefix: 'fas', iconName: "at" }).html
-const ellipsisVIcon = icon({ prefix: 'fas', iconName: "ellipsis-v" }).html
-const caretRightIcon = icon({ prefix: 'fas', iconName: "caret-right" }).html
-const questionIcon = icon({ prefix: 'far', iconName: "question-circle" }).html
-const clockIcon = icon({ prefix: 'far', iconName: "clock" }).html
-const editIcon = icon({ prefix: 'far', iconName: "edit" }).html
-const commentIcon = icon({ prefix: 'far', iconName: "comment-dots" }).html
-const chatIcon = icon({ prefix: 'far', iconName: "comments" }).html
-const bookmarkIcon = icon({ prefix: 'far', iconName: "bookmark" }).html
+const homeIcon = icon({ prefix: "fas", iconName: "home" }).html;
+const searchIcon = icon({ prefix: "fas", iconName: "search" }).html;
+const arrowDown = icon({ prefix: "fas", iconName: "chevron-down" }).html;
+const atIcon = icon({ prefix: "fas", iconName: "at" }).html;
+const ellipsisVIcon = icon({ prefix: "fas", iconName: "ellipsis-v" }).html;
+const caretRightIcon = icon({ prefix: "fas", iconName: "caret-right" }).html;
+const paperclipIcon = icon({ prefix: "fas", iconName: "paperclip" }).html;
+const boltIcon = icon({ prefix: "fas", iconName: "bolt" }).html;
+const paperplaneIcon = icon({ prefix: "fas", iconName: "paper-plane" }).html;
+const questionIcon = icon({ prefix: "far", iconName: "question-circle" }).html;
+const clockIcon = icon({ prefix: "far", iconName: "clock" }).html;
+const editIcon = icon({ prefix: "far", iconName: "edit" }).html;
+const commentIcon = icon({ prefix: "far", iconName: "comment-dots" }).html;
+const chatIcon = icon({ prefix: "far", iconName: "comments" }).html;
+const bookmarkIcon = icon({ prefix: "far", iconName: "bookmark" }).html;
+const smileyIcon = icon({ prefix: "far", iconName: "smile" }).html;
 
 /* Adding GET method to homepage */
 
 router.get("/", (req, res, next) => {
-  res.render("home", { title: "Welcome to Slack (the Clone)", 
-    homeIcon: homeIcon, 
+  res.render("home", {
+    title: "Welcome to Slack (the Clone)",
+    homeIcon: homeIcon,
     questionIcon: questionIcon,
     clockIcon: clockIcon,
     searchIcon: searchIcon,
@@ -39,6 +44,10 @@ router.get("/", (req, res, next) => {
     bookmarkIcon: bookmarkIcon,
     ellipsisVIcon: ellipsisVIcon,
     caretRightIcon: caretRightIcon,
+    smileyIcon: smileyIcon,
+    paperclipIcon: paperclipIcon,
+    boltIcon: boltIcon,
+    paperplaneIcon: paperplaneIcon,
   });
 });
 
