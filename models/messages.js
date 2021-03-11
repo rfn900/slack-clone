5624e27f29eec0ec7de98d2f6737c75527681a95
rfn900 <rfn900@gmail.com>
2021-03-11 13:43:43 +0100

    Add mongoose db model for chat messages

---
+++ models/messages.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const MessagesSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+
+  userId: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Messages = mongoose.model("Messages", MessagesSchema);
+
+module.exports = Messages;
