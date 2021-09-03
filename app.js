9a513734fda62db93ab922a7bf6b3e99fb6ce767
rfn900 <rfn900@gmail.com>
2021-09-03 13:03:34 +0200

    Use env variable for port

--- app.js
+++ app.js
@@ -184,6 +184,6 @@ io.on("connection", (socket) => {
 
 // Server starts listening:
 const port = process.env.PORT || 3000;
-http.listen(3000, () =>
+http.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
